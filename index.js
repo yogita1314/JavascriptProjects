1df291a8244221e763999b460023e41538e4f087
yogita1314 <118342939+yogita1314@users.noreply.github.com>
2022-11-23 11:52:07 +0530

    index.js chang

--- index.js
+++ index.js
@@ -4,52 +4,52 @@ const projects = [
   {
     image: "images/p1.png",
     title: "Life Timer App",
-    url: "../project1/index.html",
+    url: "project1/index.html",
   },
   {
     image: "images/p2.png",
     title: "Github User Search App",
-    url: "../project2/index.html",
+    url: "project2/index.html",
   },
   {
     image: "images/p3.png",
     title: "Percentage Calculator App",
-    url: "../project3/index.html",
+    url: "project3/index.html",
   },
   {
     image: "images/p4.png",
     title: "Math Quiz App",
-    url: "../project4/index.html",
+    url: "project4/index.html",
   },
   {
     image: "images/p5.png",
     title: "Search & Filters Product App",
-    url: "../project5/index.html",
+    url: "project5/index.html",
   },
   {
     image: "images/p6.png",
     title: "Color Generator App",
-    url: "../project6/index.html",
+    url: "project6/index.html",
   },
   {
     image: "images/p7.png",
     title: "QR Code Generator App",
-    url: "../project7/index.html",
+    url: "project7/index.html",
   },
   {
     image: "images/p8.png",
     title: "Expense Tracker App",
-    url: "../project8/index.html",
+    url: "project8/index.html",
   },
   {
     image: "images/p9.png",
     title: "Password Generator App",
-    url: "../project9/index.html",
+    url: "project9/index.html",
   },
   {
     image: "images/p10.png",
     title: "Drawer + Dropdown",
-    url: "../project10/index.html",
+    url: "project10/index.html",
   },
 ];
 
